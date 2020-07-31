7ba8c9930e64f8c314ebe6d6da142f24ae150009
Internet-today <61218439+Internet-today@users.noreply.github.com>
2020-07-31 14:30:03 +0900

    Hot. Initial revision.

---
+++ CoinToss.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CoinToss {
+	public static void main(String args[]) {
+		
+		System.out.println("Tossing a coin...");
+		
+		String[] Result = {"Heads","Tails"};
+		int[] resultValue = {0,0};
+		int Toss = 0;
+		for (int i = 1; i < 6; i++) {
+			
+			Toss = (int)(Math.random() * ((1 - 0) + 1));
+
+			System.out.println("Round" +i+":" +Result[Toss]);
+			resultValue[Toss] = resultValue[Toss] + 1;
+			
+		}
+		
+		System.out.println(Result[0]+":" +resultValue[0]+ "," +Result[1]+":" +resultValue[1]);
+		
+	}
+
+}
